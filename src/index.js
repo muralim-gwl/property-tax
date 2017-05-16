db847cf424aceac16e522ad76370d98a2ca5a97f
murali m <muralim4242@gmail.com>
2017-05-16 15:17:45 +0530

    Added common css file and images

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import store from './store';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // import './index.css';
+import './styles/application.css';
+import './styles/vistyle.css';
+
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
